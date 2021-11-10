1779c1a7353541aab494d377ee769f7483782c49
Ashleigh Jacobs <ashleigh.m.jacobs1@gmail.com>
2021-11-10 20:42:40 +0000

    Angular routing and changed theme

--- client/src/app/app.module.ts
+++ client/src/app/app.module.ts
@@ -10,13 +10,25 @@ import { FormsModule } from '@angular/forms';
 import { BsDropdownModule } from 'ngx-bootstrap/dropdown';
 import { HomeComponent } from './home/home.component';
 import { RegisterComponent } from './register/register.component';
+import { MemberListComponent } from './members/member-list/member-list.component';
+import { MemberDetailComponent } from './members/member-detail/member-detail.component';
+import { ListsComponent } from './profile/lists.component';
+import { MessagesComponent } from './messages/messages.component';
+import { FeedComponent } from './feed/feed.component';
+import { ToastrModule } from 'ngx-toastr';
+import { SharedModule } from './_modules/shared.module';
 
 @NgModule({
   declarations: [
     AppComponent,
     NavComponent,
     HomeComponent,
-    RegisterComponent
+    RegisterComponent,
+    MemberListComponent,
+    MemberDetailComponent,
+    ListsComponent,
+    MessagesComponent,
+    FeedComponent
   ],
   imports: [
     BrowserModule,
@@ -24,7 +36,7 @@ import { RegisterComponent } from './register/register.component';
     HttpClientModule,
     BrowserAnimationsModule,
     FormsModule,
-    BsDropdownModule.forRoot()
+    SharedModule
   ],
   providers: [],
   bootstrap: [AppComponent]
